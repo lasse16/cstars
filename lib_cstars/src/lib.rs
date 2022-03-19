94625d78d726d520ac873597c295e0ece83bc6c3
Lasse Haffke <lasse@haffke.me>
2022-03-19 02:36:30 +0100

    Extract non-cli specific code into library inside cargo workspace

---
+++ lib_cstars/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod commands;
+pub mod http;
+pub mod shared;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-mod commands;
-mod http;
-mod shared;
-use shared::Date;
+use lib_cstars::commands;
+use lib_cstars::http;
+use lib_cstars::shared;
+use lib_cstars::shared::Date;
 
-fn main() -> Result<(), reqwest::Error> {
-    let client = http::build_client()?;
+fn main() {
+    let client = http::build_client().unwrap();
     let result: String =
-        commands::get_description_for_date(client, Date { day: 8, year: 2020 }, 1)?;
+        commands::get_description_for_date(client, Date { day: 8, year: 2020 }, 1).unwrap();
     output_result(result);
-    Ok(())
 }
 
 fn output_result(result: String) {
