24515b8bfdcf821548d00c15bc52e81177438282
Lasse Haffke <lasse@haffke.me>
2022-03-21 21:22:21 +0100

    Add custom error to commands

--- lib_cstars/src/errors.rs
+++ lib_cstars/src/errors.rs
@@ -12,3 +12,20 @@ impl std::fmt::Display for ConfigurationError {
         write!(f, "Configuration error")
     }
 }
+
+#[derive(Debug)]
+pub struct ConnectionError {}
+
+impl Error for ConnectionError {}
+impl error::Error for ConnectionError {}
+impl std::fmt::Display for ConnectionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Connection error")
+    }
+}
+
+impl From<reqwest::Error> for ConnectionError {
+    fn from(_: reqwest::Error) -> Self {
+        ConnectionError {}
+    }
+}
