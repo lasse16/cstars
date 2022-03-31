88ad3471f72d25e46c6b551609ab5440fe08fa2d
Lasse Haffke <lasse@haffke.me>
2022-03-31 22:54:55 +0200

    Add toml configuration source

--- lib_cstars/src/configuration.rs
+++ lib_cstars/src/configuration.rs
@@ -1,10 +1,10 @@
 use figment::providers::{Format, Toml};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::path::Path;
 
 use crate::errors::Error;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Configuration {
     session_cookie_retrieval_command: String,
 }
@@ -17,7 +17,26 @@ impl Default for Configuration {
     }
 }
 
+impl figment::Provider for Configuration {
+    fn metadata(&self) -> figment::Metadata {
+        figment::Metadata::named("lib_cstars configuration")
+    }
+
+    fn data(
+        &self,
+    ) -> Result<figment::value::Map<figment::Profile, figment::value::Dict>, figment::Error> {
+        figment::providers::Serialized::defaults(Configuration::default()).data()
+    }
+}
+
 pub fn parse_configuration(toml_file_location: &Path) -> Result<Configuration, Error> {
     let default_configuration = Configuration::default();
-    Ok(default_configuration)
+    let config: Configuration = figment::Figment::from(default_configuration)
+        .merge(Toml::file(toml_file_location))
+        .extract()
+        .map_err(|err| Error::ConfigurationError {
+            message: err.to_string(),
+        })?;
+
+    Ok(config)
 }
